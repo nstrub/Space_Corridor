8b95d975699c02341de5d55f78776fabfb18aaa4
nico <strub17u@etu.univ-lorraine.fr>
2021-04-06 21:43:25 +0200

    Ajout 5, 6 et background

--- Projet/Niveau 1/tests/main.c
+++ Projet/Niveau 1/tests/main.c
@@ -240,29 +240,52 @@ void update_data(world_t *world){
 
 void handle_events(SDL_Event *event,world_t *world){
     Uint8 *keystates;
-    while( SDL_PollEvent( event ) ) {
+    while( SDL_PollEvent( event ) ) 
+    {
         
         //Si l'utilisateur a cliqué sur le X de la fenêtre
         if( event->type == SDL_QUIT ) {
             //On indique la fin du jeu
             world->gameover = 1;
         }
-       
-         //si une touche est appuyée
-         if(event->type == SDL_KEYDOWN){
+
+        //si une touche est appuyée
+        
+        //SI LA TOUCHE ECHAP EST APPUYEE (quitter le jeu)
+         
+         if(event->type == SDL_KEYDOWN)
+         {
              //si la touche appuyée est 'D'
-            if(event->key.keysym.sym == SDLK_d){
+            if(event->key.keysym.sym == SDLK_d)
+            {
                  printf("La touche D est appuyée\n");
             }
             //si Flèche droite est appuyée
-            if(event->key.keysym.sym == SDLK_RIGHT){
-                 world->vaisseau.x += 1;
+            if(event->key.keysym.sym == SDLK_RIGHT)
+            {
+                 world->vaisseau.x += 6;
             }
             //si Flèche gauche est appuyée
-            if(event->key.keysym.sym == SDLK_LEFT){
-                 world->vaisseau.x += -1;
+            if(event->key.keysym.sym == SDLK_LEFT)
+            {
+                 world->vaisseau.x += -6;
             }
-         }
+            //si la touche échap est appuyée
+            if(event->key.keysym.sym == SDLK_ESCAPE)
+            {
+                world->gameover = 1;
+            }
+            //si la flèche du haut est appuyée
+            if(event->key.keysym.sym == SDLK_UP)
+            {
+                world->vitesse += 2;
+            }
+            //si la flèche du haut est appuyée
+            if(event->key.keysym.sym == SDLK_DOWN)
+            {
+                world->vitesse -= 2;
+            }
+        }
     }
 }
 
@@ -289,7 +312,7 @@ void clean_textures(textures_t *textures){
 */
 
 void  init_textures(SDL_Renderer *renderer, textures_t *textures){
-    textures->background = load_image( "ressources/background.bmp",renderer);
+    textures->background = load_image( "ressources/space-background.bmp",renderer);
     textures->vaisseau = load_image( "ressources/spaceship.bmp", renderer);
     textures->arrivee = load_image( "ressources/finish_line.bmp", renderer);
     textures->meteorite = load_image( "ressources/meteorite.bmp", renderer);
