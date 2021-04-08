64a9efdf6ab33ad8eadc2b4aa7612bb8963aae90
demang179u <dan.demange7@etu.univ-lorraine.fr>
2021-04-08 12:16:58 +0200

    Niv 2 terminé

---
+++ Projet/Niveau 2/Programme/tests.c
@@ -0,0 +1,32 @@
+/**
+ * \file param.h
+ * \brief Fichier de test pour le 2) du niveau 2
+ * \author STRUB Nicolas DEMANGE Dan
+ * \version 1.0
+ * \date 8 avril 2021
+ */
+
+#include "Modules/monde.h"
+#include "Modules/param.h"
+#include "Modules/graphics.h"
+
+
+//void test_init_sprite_param(sprite_t *sprite, int x, int y, int w, int h){
+//    init_sprite(&sprite,x,y,w,h);
+//}
+
+//void test_init_sprite(){
+//    test_init_sprite_param(&sprite,0,0,0,0);
+//    test_init_sprite_param(&sprite,50,50,10,10);
+//    test_init_sprite_param(&sprite,0,100,25,75);
+//    test_init_sprite_param(&sprite,100,0,75,25);
+//    test_init_sprite_param(&sprite,100,100,100,100);
+//}
+
+
+int main( int argc, char* args[] ){
+    //test_init_sprite();
+    
+    
+    return 0;
+}
