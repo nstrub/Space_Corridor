b941d04b8e53db8d20b8c6fc6ff6efa111b9974f
strub17u <strub17u@etu.univ-lorraine.fr>
2021-04-08 10:51:35 +0200

    init_sprite_param (pas complet)

--- Projet/Niveau 2/Programme/Tests/tests.c
+++ Projet/Niveau 2/Programme/Tests/tests.c
@@ -10,42 +10,15 @@
 #include "../Modules/param.h"
 #include "../Modules/graphics.h"
 
-void test_init_sprite(sprite_t *sprite)
-{
 
+void test_init_sprite_param(sprite_t *sprite, int x, int y, int w, int h){
+    init_sprite(&sprite,x,y,w,h);
 }
 
 
 
 int main( int argc, char* args[] )
 {
-    SDL_Event event;
-    world_t world;
-    textures_t textures;
-    SDL_Renderer *renderer;
-    SDL_Window *window;
 
-    //initialisation du jeu
-    init(&window,&renderer,&textures,&world);
-    
-    while(!is_game_over(&world)){ //tant que le jeu n'est pas fini
-        
-        //gestion des évènements
-        handle_events(&event,&world);
-        
-        //mise à jour des données liée à la physique du monde
-        update_data(&world);
-        
-        //rafraichissement de l'écran
-        refresh_graphics(renderer,&world,&textures);
-        
-        // pause de 10 ms pour controler la vitesse de rafraichissement
-        pause(10);
-    }
-    
-    //nettoyage final
-    clean(window,renderer,&textures,&world);
-    
-    
     return 0;
 }
