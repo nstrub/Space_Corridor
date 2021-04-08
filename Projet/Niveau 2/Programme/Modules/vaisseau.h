22dc18593a55b6672994ad355e8f59378f48ba1c
demang179u <dan.demange7@etu.univ-lorraine.fr>
2021-04-08 12:30:59 +0200

    Niv 2 .3 Début

---
+++ Projet/Niveau 2/Programme/Modules/vaisseau.h
@@ -0,0 +1,32 @@
+/**
+ * \file vaisseau.h
+ * \brief Module d'interaction entre le vaisseau et l'écran
+ * \author STRUB Nicolas DEMANGE Dan
+ * \version 1.0
+ * \date 12/04/2021
+ */
+
+#ifndef VAISSEAU_H
+#define VAISSEAU_H
+
+#include "param.h"
+
+
+// A DEFINIR ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+void depacement_gauche(sprite_t *sprite);
+
+void depacement_droite(sprite_t *sprite);
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
