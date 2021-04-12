cb7dd16173a82d5eff62bdf4e863fab511e284fc
nico <strub17u@etu.univ-lorraine.fr>
2021-04-12 16:02:35 +0200

    ajout de handle event

---
+++ Projet/Niveau 2/Programme/Modules/handle_event.h
@@ -0,0 +1,17 @@
+/**
+ * \file handle_event.h
+ * \brief Module contenant le monde
+ * \author STRUB Nicolas DEMANGE Dan
+ * \version 1.0
+ * \date 7 avril 2021
+ */
+
+
+/**
+ * @brief Fonction visant à géré tout les évènements ce déroulant lors de la boucle de jeu
+ * 
+ * @param event 
+ * @param world 
+ */
+
+void handle_events(SDL_Event *event,world_t *world);
