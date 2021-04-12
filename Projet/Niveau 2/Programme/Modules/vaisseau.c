a3dd3b2ec2edd28d270510a63391b89ea400ce10
strub17u <strub17u@etu.univ-lorraine.fr>
2021-04-12 10:33:08 +0200

    Ajout du module vaisseau.c

---
+++ Projet/Niveau 2/Programme/Modules/vaisseau.c
@@ -0,0 +1,17 @@
+/**
+ * \file vaisseau.c
+ * \author Strub Nicolas Demange Dan
+ * \brief Gestion des collision et autres pour le vaisseau
+ * \version 1.0
+ * \date 12/04/2021
+ */
+
+#include "graphics.h"
+#include "monde.h"
+#include "param.h"
+#include "vaisseau.h"
+
+void depacement(sprite_t *sprite)
+{
+
+}
