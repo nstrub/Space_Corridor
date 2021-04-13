75806b2e2152b28950094b4c920e55d44733c3cf
demang179u <dan.demange7@etu.univ-lorraine.fr>
2021-04-13 10:54:03 +0200

    Niv 3 2.1 fait

--- Projet/Niveau 3/Programme/Modules/param.h
+++ Projet/Niveau 3/Programme/Modules/param.h
@@ -16,69 +16,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// DEFINITION DES STRUCTURES //
-
-/**
-*\struct sprite_s
-*\typedef sprite_t
-*\brief Représente un sprite du jeu
-*/
-struct sprite_s{
-    int x; /*!< Abscisse du centre de l'image. */
-    int y; /*!< Ordonnée du centre de l'image. */
-    int h; /*!< Hauteur de l'image.*/
-    int w; /*!< Largeur de l'image.*/
-};
-typedef struct sprite_s sprite_t;
-
-
-
-/**
- * \brief Représentation du monde du jeu
-*/
-
-struct world_s{
-    sprite_t vaisseau; /*! < Ajout d'un vaisseau de type sprite_t au monde */
-    sprite_t arrivee; /*! < Ajout de la ligne d'arrivée de type sprite_t au monde */
-    sprite_t mur; /*! <Ajout du mur de meteorite de type sprite_t au monde */
-
-    int gameover; /*!< Champ indiquant si l'on est à la fin du jeu */
-    int vitesse; /*!< Vitesse de déplacement de base des strucures*/
-    int desappear; /*!< Variable qui sert à afficher le sprite du vaisseau*/
-
-};
-
-/**
- * \brief Type qui correspond aux données du monde
- */
-
-typedef struct world_s world_t;
-
-
-
-
-/**
- * \brief Représentation pour stocker les textures nécessaires à l'affichage graphique
-*/
-
-struct textures_s{
-    SDL_Texture* background; /*!< Texture liée à l'image du fond de l'écran. */
-    SDL_Texture* vaisseau; /*!< Texture liée à l'image du vaisseau. */
-    SDL_Texture* arrivee; /*!< Texture liée à l'image de la ligne d'arrivée */
-    SDL_Texture* meteorite; /*!< Texture liée à l'image de meteorite */
-};
-
-
-/**
- * \brief Type qui correspond aux textures du jeu
-*/
-
-typedef struct textures_s textures_t;
-
-
-
-
-
+// Définitions
 /**
  * \brief Largeur de l'écran de jeu
  */
@@ -125,6 +63,77 @@ typedef struct textures_s textures_t;
 
 #define INITIAL_SPEED 2
 
+/**
+ * @brief Nombre de murs de méréorites dans le jeu
+ * 
+ */
+#define MURS_NBR 6
+
+/**
+ * @brief Nombre de murs de méréorites dans le jeu
+ * 
+ */
+#define MURS_NBR 6
+
+// DEFINITION DES STRUCTURES //
+
+/**
+*\struct sprite_s
+*\typedef sprite_t
+*\brief Représente un sprite du jeu
+*/
+struct sprite_s{
+    int x; /*!< Abscisse du centre de l'image. */
+    int y; /*!< Ordonnée du centre de l'image. */
+    int h; /*!< Hauteur de l'image.*/
+    int w; /*!< Largeur de l'image.*/
+};
+typedef struct sprite_s sprite_t;
+
+
+
+/**
+ * \brief Représentation du monde du jeu
+*/
+
+struct world_s{
+    sprite_t vaisseau; /*! < Ajout d'un vaisseau de type sprite_t au monde */
+    sprite_t arrivee; /*! < Ajout de la ligne d'arrivée de type sprite_t au monde */
+    sprite_t mur; /*! <Ajout du mur de meteorite de type sprite_t au monde */
+    sprite_t murs[MURS_NBR]; /*! <Ajout des murs de meteorites de type sprite_t au monde.*/
+
+    int gameover; /*!< Champ indiquant si l'on est à la fin du jeu */
+    int vitesse; /*!< Vitesse de déplacement de base des strucures*/
+    int desappear; /*!< Variable qui sert à afficher le sprite du vaisseau*/
+
+};
+
+/**
+ * \brief Type qui correspond aux données du monde
+ */
+
+typedef struct world_s world_t;
+
+
+
+
+/**
+ * \brief Représentation pour stocker les textures nécessaires à l'affichage graphique
+*/
+
+struct textures_s{
+    SDL_Texture* background; /*!< Texture liée à l'image du fond de l'écran. */
+    SDL_Texture* vaisseau; /*!< Texture liée à l'image du vaisseau. */
+    SDL_Texture* arrivee; /*!< Texture liée à l'image de la ligne d'arrivée */
+    SDL_Texture* meteorite; /*!< Texture liée à l'image de meteorite */
+};
+
+
+/**
+ * \brief Type qui correspond aux textures du jeu
+*/
+
+typedef struct textures_s textures_t;
 
 
 #endif
